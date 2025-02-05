0a3b9fe4b5d78b01da57a765b9543e5c968d2703
SoftOSDev <117074497+SoftOSDev@users.noreply.github.com>
2025-02-05 13:23:42 +0800

    Update index.php

--- php-framework-nette/index.php
+++ php-framework-nette/index.php
@@ -3,12 +3,12 @@
     <meta http-equiv="Content-Type" content="application/xhtml+xml; charset=utf-8; X-Wap-Proxy-Cookie=none" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     <title>聊天</title>
-    <link type="text/css" href="app/style/chat.css?v=1.2" rel="stylesheet" />
+    <link type="text/css" href="chat.css?v=1.2" rel="stylesheet" />
 </head>
 <body>
 <div class="top"><span class="name">HUA丨多人在线聊天</span></div>
 <?php
-require_once 'app/app.php';
+require_once 'app.php';
 if(empty(@$_COOKIE[KEYS.'_name'])){
 	echo '<div class="write login">                
                 <input type="text" value="'.rand_nick().'" id="nick" maxlength="5" placeholder="请随便输入昵称.." />                
@@ -22,7 +22,7 @@ echo '<div class="talk mk-chat-box">
                 <a href="javascript:;" id="send" class="write-link send" onclick="send()">发送</a>
 </div>'; 
 ?>
-<script src="app/style/jquery.min.js"></script>
-<script src="app/style/chat.min.js"></script>
+<script src="jquery.min.js"></script>
+<script src="chat.min.js"></script>
 </body>
 </html>
